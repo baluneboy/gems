3b042998ae89bc2c56124b45f19285103b0ff640
Ken <none>
2017-05-08 16:24:24 -0400

    From home, a refined find.

--- sketchupsams/examples/03_zoom_sensor/ex_zoom_sensor/main.rb
+++ sketchupsams/examples/03_zoom_sensor/ex_zoom_sensor/main.rb
@@ -27,7 +27,7 @@ class Geom::Transformation
     logger.info("#{str3}")
     str4 = sprintf l,  a[12], a[13], a[14], a[15]
     logger.info("#{str4}")
-  end  
+  end
 end
 
 module Examples
@@ -35,18 +35,19 @@ module Examples
 
     def self._zoomsensor(sensor, logger)
 
-      sensor_name = sensor[0].upcase
-      sensor_type = sensor[1].upcase
-      sensor_def  = "SAMS_#{sensor_type}"
+      sensor_name = sensor[0].upcase      # LIKE "F12"
+      sensor_type = sensor[1].upcase      # LIKE "SE"
+      sensor_def  = "SAMS_#{sensor_type}" # LIKE "SAMS_SE"
       logger.debug("Finding #{sensor_def}-#{sensor_name}")
 
       dlist = Sketchup.active_model.definitions
       cdef = dlist[sensor_def]
       if cdef # test for nil (not found)
         if !cdef.instances.empty?
+          # find object with name LIKE "SAMS_SE-F12"
           obj = cdef.instances.find {|i| i.name == "#{sensor_def}-#{sensor_name}" }
           puts "found #{sensor_def}-#{sensor_name}" unless obj.nil?
-          logger.info("Found   #{sensor_def}-#{sensor_name}") unless obj.nil?     
+          logger.info("Found   #{sensor_def}-#{sensor_name}") unless obj.nil?
           #b = obj.bounds unless obj.nil?
           t = obj.transformation unless obj.nil?
         else
@@ -69,10 +70,11 @@ module Examples
       #z = t.zaxis unless obj.nil?
 
       # Show sensor transform matrix
-      str_mat = t.to_matrix
-      UI.messagebox "#{str_mat}" unless obj.nil?
-      #logger.info("#{str_mat}")
-      t.to_matrix_log(logger)     
+      #str_mat = t.to_matrix
+      #UI.messagebox "#{str_mat}" unless obj.nil?
+
+      # Write sensor transform info to log file
+      t.to_matrix_log(logger)
 
     end
 
@@ -104,20 +106,21 @@ module Examples
         logger.info("ZOOMING ALL")
         (0..5).each { |f|
           (0..7).each { |p|
-            logger.info("SAMS_SE-F#{f+1}#{p}")
-            self._zoomsensor(["#{f+1}#{p}", "SE"], logger)
+            inp = ["F#{f+1}#{p}", "SE"] # LIKE ["F37", "SE"]
+            logger.info(inp)
+            self._zoomsensor(inp, logger)
           }
         }
-        
+
       else
 
-        # Call our new method.
+        # Call our one-at-a-time method
         if user_input
           self._zoomsensor(user_input, logger)
         else
           puts "User hit cancel."
         end
-      
+
       end
 
       logger.info("Program end")
--- sketchupsams/readme_ken.txt
+++ sketchupsams/readme_ken.txt
@@ -1,3 +1,10 @@
+# Created a ruby file in Plugins folder named:
+/Users/ken/Library/Application Support/SketchUp 2017/SketchUp/Plugins/load_sams.rb
+# where load_sams.rb has these next 2 lines:
+$LOAD_PATH << '/Users/ken/dev/programs/ruby/gems/sketchupsams'
+require 'load_examples.rb'
+
+OLDER README STUFF:
 Put a tag on component and use ruby to query position by tag.
 Place a tagged component from file with expected orientation.
 Use ruby to get orientation and location info on tagged component.
